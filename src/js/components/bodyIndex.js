f92f1c07198a5f116cc48c961c7789b6fb626078
USERCHI-F39IHR7\Administrator <yonggangliang@sina.cn>
2018-01-23 17:41:07 +0800

    事件与数据双向绑定

--- src/js/components/bodyIndex.js
+++ src/js/components/bodyIndex.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BodyChild from './bodyChild'
 export default class BodyIndex extends React.Component{
     componentWillMount () {
         console.log('willMount')
@@ -9,7 +10,13 @@ export default class BodyIndex extends React.Component{
     }
     constructor () {
         super () // 调用基类的所有的初始化方法
-        this.state = {username: 'Parry'}
+        this.state = {username: 'Parry', age: 18}
+    }
+    changeUserInfo () {
+        this.setState({username: '改了名字'})
+    }
+    handleChildValueChange (event) {
+        this.setState({age: event.target.value})
     }
     render () {
         setTimeout(() => {
@@ -18,8 +25,11 @@ export default class BodyIndex extends React.Component{
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{this.state.username}</p>
+                <p>name:{this.state.username}</p>
+                <p>age:{this.state.age}</p>
                 <p>用户ID：{this.props.userId}</p>
+                <button onClick={this.changeUserInfo.bind(this)}>更名</button>
+                <BodyChild handleChildValueChange={this.handleChildValueChange.bind(this)}></BodyChild>
             </div>
         )
     }
