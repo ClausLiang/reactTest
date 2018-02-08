13b46394de56740c13599d49fd1d7d11e244f1db
liangyonggang <yonggangliang@sina.cn>
2018-02-08 16:05:25 +0800

    内联样式及内联样式中的表达式

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,8 +1,27 @@
 import React from 'react'
 export default class ComponentHeader extends React.Component{
+    constructor () {
+        super()
+        this.state = {
+            miniHeader: false
+        }
+    }
+    switchHeader () {
+        this.setState({
+            miniHeader: !this.state.miniHeader
+        })
+    }
     render () {
+        const styleComponentHeader = {
+            header: {
+                backgroundColor: "#333",
+                color: "#fff",
+                paddingTop: this.state.miniHeader ? "3px" : "15px",
+                paddingBottom: this.state.miniHeader ? "3px" : "15px"
+            }
+        }
         return (
-            <header>
+            <header style={styleComponentHeader.header} className="smallFontSize" onClick={this.switchHeader.bind(this)}>
                 <h1>这里是头部</h1>
             </header>
         )
